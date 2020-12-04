13e1093a7a4bf41be1633d43f54e5be3096964dd
HaloC23 <69718744+HaloC23@users.noreply.github.com>
2020-12-04 18:23:40 +0000

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    date: "Friday 4th Sept",
+    description: "Cloudy",
+    temperature: " 14",
+    time: "16:23",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+  };
+
+  return (
+    <div className="Weather">
+      <div className="row">
+        <div className="col">
+          <div className="header">
+            <img src={weatherData.imgUrl} className="icon" />
+            <h1 className="Main-city">{weatherData.city}</h1>
+            <h2>
+              {weatherData.date}
+              <div className="time">{weatherData.time}</div>
+            </h2>
+            <p className="Description1">{weatherData.description}y</p>
+
+            <div>
+              {" "}
+              <strong className="hightemp">{weatherData.temperature}</strong>
+              <small>
+                {" "}
+                <a href="/" className="celsius-link">
+                  °C{" "}
+                </a>{" "}
+                |{" "}
+                <a href="/" className="fahrenheit-link">
+                  °F{" "}
+                </a>{" "}
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Weather from ".Weather.js";
+import App from "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);

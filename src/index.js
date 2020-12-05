aa8f566cac0ed2d6a9c06656311b912584d2be6d
HaloC23 <69718744+HaloC23@users.noreply.github.com>
2020-12-05 12:32:10 +0000

    Edited  index.js removed "./App" as was incorrect.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Weather from ".Weather.js";
-import App from "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
-}
-
+import Weather from "./Weather";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Weather />
   </React.StrictMode>,
   rootElement
 );

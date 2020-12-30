f6ca47d0688225527f2abc063d6b6e8a6faf3ed0
HC23 <69718744+HaloC23@users.noreply.github.com>
2020-12-30 18:30:13 +0000

    Added a Footer component Component detailed the source code and linked to GitHub

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <h4>
+          {" "}
+          Coded by Hannah <a href="https://github.com/HaloC23">
+            {" "}
+            over here!{" "}
+          </a>{" "}
+        </h4>
+      </footer>
+    </div>
+  );
+}
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
+import Footer from "./Components/Footer";
 import SearchEngine from "./Components/SearchEngine";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <Weather />
     <SearchEngine />
+    <Footer />
   </React.StrictMode>,
   rootElement
 );

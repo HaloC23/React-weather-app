2c51a6ceb32d1e9103b460a97b391b312080c5cd
HC23 <69718744+HaloC23@users.noreply.github.com>
2021-01-05 18:43:03 +0000

    Created a conversion link  For Fahrenheit & back to Celsius

--- src/Components/WeatherDisplay.js
+++ src/Components/WeatherDisplay.js
@@ -10,27 +10,22 @@ export default function WeatherDisplay(props) {
         <div className="col">
           <div className="header">
             <div className="float-left">
+              
               <Icon code={props.data.icon} />
+            
             </div>
 
             <h1 className="Main-city">{props.data.city}</h1>
             <h2>
+              
               <FormattedDate date={props.data.date} />
+             
               <div className="time">{props.data.time}</div>
             </h2>
             <p className="MainDescription">{props.data.description}</p>
-
-            <div>
-              <strong className="hightemp">{props.data.temperature}</strong>{" "}
-              <a href="/" className="celsius-link">
-                °C{" "}
-              </a>{" "}
-              |{" "}
-              <a href="/" className="fahrenheit-link">
-                {" "}
-                °F{" "}
-              </a>{" "}
-            </div>
+              
+              <WeatherTemp  celsius ={props.data.temperature/> 
+           
           </div>
         </div>
       </div>

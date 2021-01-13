3ba37f30742ff9cb185376b8b943f3701ed01a3f
HC23 <69718744+HaloC23@users.noreply.github.com>
2021-01-13 17:45:04 +0000

    Added a background image Background image into the weather display component and added styling.

--- src/Components/WeatherDisplay.js
+++ src/Components/WeatherDisplay.js
@@ -5,20 +5,22 @@ import WeatherTemp from "./WeatherTemp";
 
 export default function WeatherDisplay(props) {
   return (
-    <div className="WeatherDisplay">
-      <div className="row">
-        <Icon code={props.data.icon} />
-      </div>
+    <div class="bg-img">
+      <div className="WeatherDisplay">
+        <div className="row">
+          <Icon code={props.data.icon} />
+        </div>
 
-      <div className="col">
-        <div className="header">
-          <h1 className="MainCity">{props.data.city}</h1>
-          <h2>
-            <FormattedDate date={props.data.date} />
-          </h2>
-          <p className="MainDescription">{props.data.description}</p>
+        <div className="col">
+          <div className="header">
+            <h1 className="MainCity">{props.data.city}</h1>
+            <h2>
+              <FormattedDate date={props.data.date} />
+            </h2>
+            <p className="MainDescription">{props.data.description}</p>
 
-          <WeatherTemp celsius={props.data.temperature} />
+            <WeatherTemp celsius={props.data.temperature} />
+          </div>
         </div>
       </div>
     </div>
